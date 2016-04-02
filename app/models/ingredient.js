105702a290d246549207fbb164b319794673f0bc
Jackson Cafazzo <doctorjuno@gmail.com>
2016-04-02 10:03:42 -0700

    remove all files for gh-pages build

--- app/models/ingredient.js
+++
@@ -1,9 +0,0 @@
-import DS from 'ember-data';
-
-export default DS.Model.extend({
-  measurement: DS.attr(),
-  units: DS.attr(),
-  name: DS.attr(),
-  source: DS.attr(),
-  recipe: DS.belongsTo('recipe', { async: true })
-});
